6934d7b53c6212237541cb94af6361c8a9a3a42e
Julio Barrios <julio.barrios.9618@gmail.com>
2022-08-12 16:32:56 -0300

    Fix deploy

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,8 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-    publicPath: "/",
+    filename: "main.js",
+    publicPath: "./",
   },
   resolve: {
     extensions: [".js", ".jsx"],
